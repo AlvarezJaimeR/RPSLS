59a8bf64a56880b751c28c5d790c857f75acc393
AlvarezJaimeR <jrralvarez@gmail.com>
2021-03-29 20:57:07 -0400

    added a user validation for the user input when they pick their opponent. Also added displayRules

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 "use strict"
 const Player = require('./player');
+const prompt = require('prompt-sync')();
 
 class Game {
     constructor(){
@@ -8,10 +9,37 @@ class Game {
 
     runGame(){ //"main"
         this.displayRules();
+        this.pickOpponent();
     }
 
     displayRules(){
         console.log("Welcome to Rock Paper Scissors Lizard Spock!");
+        console.log('Best of three will decide the winner.');
+        console.log('You may decide to vs another human or AI.');
+    }
+
+    pickOpponent(){
+        console.log('Would you like to vs another Human or an AI?:');
+        let userOpponentChoice = prompt().toLowerCase();
+        let pass = this.userOpponentValidation(userOpponentChoice);
+        console.log(pass);
+    }
+
+    userOpponentValidation(userOppChoice){
+        var letterCheck = /^[A-Za-z]+$/;
+        if (userOppChoice==null || userOppChoice=="")
+        {
+            console.log('Please input your choice of human or AI');
+            return false;
+        }
+        else if (!userOppChoice.match(letterCheck))
+        {
+            console.log("Choice must contain letters only!");
+            return false;
+        }
+        else {
+        return true;
+        }
     }
 }
 
