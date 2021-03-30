a1197d4da350663feb59c2ea041b58edc63d5a01
AlvarezJaimeR <jrralvarez@gmail.com>
2021-03-29 21:42:38 -0400

    able to pick between human and AI. user validation works for numbers and invalid characters

--- game.js
+++ game.js
@@ -21,10 +21,35 @@ class Game {
     pickOpponent(){
         console.log('Would you like to vs another Human or an AI?:');
         let userOpponentChoice = prompt().toLowerCase();
-        let pass = this.userOpponentValidation(userOpponentChoice);
-        console.log(pass);
+        let test = false;
+        while (test === false){
+            test = this.userOpponentValidation(userOpponentChoice);
+            console.log(test);
+            if (test === false){
+                userOpponentChoice = prompt().toLowerCase();
+            }
+            else{
+                break;
+            }
+        }
+        switch (userOpponentChoice){
+            case 'human':
+                console.log('You picked your opponent to be another human. Please enter their name.');
+                let playerTwoName = prompt();
+                this.playerTwo = new Player(playerTwoName);
+                console.log(playerTwo.name);
+                break;
+            case 'ai':
+                console.log('You picked your opponent to be an AI. Good luck!');
+                break;
+            default: {
+                console.log('Pick human or Ai.');
+                return this.pickOpponent();
+            }
+        }return userOpponentChoice;
     }
 
+    //Check if the user input is all characters
     userOpponentValidation(userOppChoice){
         var letterCheck = /^[A-Za-z]+$/;
         if (userOppChoice==null || userOppChoice=="")
@@ -35,10 +60,11 @@ class Game {
         else if (!userOppChoice.match(letterCheck))
         {
             console.log("Choice must contain letters only!");
+            console.log('Please choose between human or AI');
             return false;
         }
         else {
-        return true;
+            return true;
         }
     }
 }
